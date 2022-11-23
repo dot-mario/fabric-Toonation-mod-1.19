b28efedcc54b0b93a3be5b7f950c9c0cf62ad575
kbh1933 <kbh1933@gmail.com>
2022-11-23 20:48:23 +0900

    added important comment

--- src/main/java/com/dotmario/toonation/ToonationMod.java
+++ src/main/java/com/dotmario/toonation/ToonationMod.java
@@ -1,10 +1,13 @@
 package com.dotmario.toonation;
 
+import com.dotmario.toonation.config.MidnightConfigExample;
 import com.dotmario.toonation.networking.ModMessages;
+import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.LoggerFactory;
 
 import java.util.logging.Logger;
+
 // Very important comment
 public class ToonationMod implements ModInitializer {
 
@@ -14,6 +17,9 @@ public class ToonationMod implements ModInitializer {
     @Override
     public void onInitialize() {
         LOGGER.info("Hello Fabric world!");
+//        ModConfigs.registerConfigs();
+        MidnightConfig.init(MOD_ID, MidnightConfigExample.class);
+
         ModMessages.registerC2SPackets();
     }
 }
