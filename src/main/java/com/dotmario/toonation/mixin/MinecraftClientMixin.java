cae988e4b6eea84507bf7e9b5068e95cbe1db21b
kbh1933 <kbh1933@gmail.com>
2022-12-13 19:47:35 +0900

    test

--- src/main/java/com/dotmario/toonation/mixin/MinecraftClientMixin.java
+++ src/main/java/com/dotmario/toonation/mixin/MinecraftClientMixin.java
@@ -2,10 +2,7 @@ package com.dotmario.toonation.mixin;
 
 import com.dotmario.toonation.Donation.DonationCheck;
 import net.minecraft.client.MinecraftClient;
-import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,8 +10,6 @@ import static com.dotmario.toonation.ToonationMod.LOGGER;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
-
-    @Shadow @Final private static Logger LOGGER;
 
     @Inject(method = "close",at = @At("HEAD"))
     public void injectClose(CallbackInfo ci) {
